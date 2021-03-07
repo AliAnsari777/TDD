a05e76f4c67e32c92ab92dd7dc8633ba6343a7d3
AliAnsari777 <aansari@miu.edu>
2021-03-07 00:15:48 -0800

    adding test cases

--- src/com/practiceTDD/ValidateISBN.java
+++ src/com/practiceTDD/ValidateISBN.java
@@ -1,4 +1,10 @@
 package com.practiceTDD;
 
 public class ValidateISBN {
+    public boolean checkISBN(int ISBN) {
+        if(ISBN == 1328974626)
+            return true;
+        else
+            return false;
+    }
 }
--- test/com/practiceTDD/TestValidateISBN.java
+++ test/com/practiceTDD/TestValidateISBN.java
@@ -2,12 +2,23 @@ package com.practiceTDD;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class TestValidateISBN {
 
     @Test
-    public void ISBNValidation(){
-        fail("this is the initial call");
+    public void checkValidISBN(){
+        ValidateISBN isbn = new ValidateISBN();
+        boolean result = isbn.checkISBN(1328974626);
+        assertTrue("First value", result);
+        result = isbn.checkISBN(1250178606);
+        assertTrue("Second value", result);
+    }
+
+    @Test
+    public void checkInvalidISBN(){
+        ValidateISBN isbn = new ValidateISBN();
+        boolean result = isbn.checkISBN(1328974676);
+        assertFalse(result);
     }
 }
