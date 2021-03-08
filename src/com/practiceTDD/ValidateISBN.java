9f4e151af66fa1b059d0daeda15e3f2ee6391d31
AliAnsari777 <aansari@miu.edu>
2021-03-07 20:37:53 -0800

    end of chapter five

--- src/com/practiceTDD/ValidateISBN.java
+++ src/com/practiceTDD/ValidateISBN.java
@@ -3,10 +3,51 @@ package com.practiceTDD;
 public class ValidateISBN {
     public boolean checkISBN(String ISBN) {
         int total = 0;
-        for (int i = 0; i < 10; i++) {
-            total += ISBN.charAt(i) * (10 - i);
+
+        if (ISBN.length() == 13) {
+
+            return validate13DigitsISBN(ISBN, total);
+
+        } else {
+
+            return validate10DigitsISBN(ISBN, total);
         }
+    }
+
+    private boolean validate10DigitsISBN(String ISBN, int total) {
+        if (ISBN.length() != 10) {
+            throw new NumberFormatException("ISBN should be 10 digit longs");
+        }
+
+
+        for (int i = 0; i < 10; i++) {
+            if (!Character.isDigit(ISBN.charAt(i))) {
+                if (i == 9 && ISBN.charAt(i) == 'X') {
+                    total += 10;
+                } else {
+                    throw new NumberFormatException();
+                }
+            } else {
+                total += Character.getNumericValue(ISBN.charAt(i)) * (10 - i);
+            }
+        }
+
         if (total % 11 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean validate13DigitsISBN(String ISBN, int total) {
+        for (int i = 0; i < 13; i++) {
+            if (i % 2 == 0){
+                total += Character.getNumericValue(ISBN.charAt(i)) * 1;
+            }else{
+                total += Character.getNumericValue(ISBN.charAt(i)) * 3;
+            }
+        }
+
+        if (total % 10 == 0)
             return true;
         else
             return false;
--- test/com/practiceTDD/TestValidateISBN.java
+++ test/com/practiceTDD/TestValidateISBN.java
@@ -16,9 +16,43 @@ public class TestValidateISBN {
     }
 
     @Test
+    public void check13DigitValidISBN(){
+        ValidateISBN isbn = new ValidateISBN();
+        boolean result = isbn.checkISBN("9780394800011");
+        assertTrue(result, "First value");
+        result = isbn.checkISBN("9780394800387");
+        assertTrue(result, "Second value");
+    }
+
+    @Test
+    public void checkValidISBNendsWithX(){
+        ValidateISBN isbn = new ValidateISBN();
+        boolean result = isbn.checkISBN("012000030X");
+        assertTrue(result, "ISBN number ending with X is valid");
+        result = isbn.checkISBN("039480001X");
+        assertTrue(result, "ISBN number ending with X is valid");
+    }
+
+    @Test
     public void checkInvalidISBN(){
         ValidateISBN isbn = new ValidateISBN();
         boolean result = isbn.checkISBN("1328974676");
         assertFalse(result);
+    }
+
+    @Test
+    public void isISBNtenDigigt(){
+        ValidateISBN isbn = new ValidateISBN();
+        assertThrows(NumberFormatException.class, () ->{
+            isbn.checkISBN("123456789");
+        });
+    }
+
+    @Test
+    public void checkISBNIsNumber(){
+        ValidateISBN isbn = new ValidateISBN();
+        assertThrows(NumberFormatException.class, () ->{
+            isbn.checkISBN("helloworld");
+        });
     }
 }
