ea23f18ec0c6c735df6241e43fb3baf41994e7d0
AliAnsari777 <alidk2013@gmail.com>
2021-03-11 00:23:35 -0800

    completed the Queue test

--- src/com/practiceTDD/Queue.java
+++ src/com/practiceTDD/Queue.java
@@ -4,11 +4,35 @@ public class Queue {
     int[] values;
     int rare, front;
 
+    Queue(int size){
+        values = new int[size];
+        rare = 0;
+        front = 0;
+    }
+
+    public boolean enqueue(int value){
+        if (rare == values.length){
+            return false;
+        }else{
+            values[rare] = value;
+            rare++;
+            return true;
+        }
+    }
 
     public int peek(){
         if(values.length == 0)
             return 0;
         else
             return values[front];
+    }
+
+    public int dequeue(){
+        if (rare == front){
+            throw new NullPointerException();
+        }
+        int value = values[front];
+        front++;
+        return value;
     }
 }
--- test/com/practiceTDD/TestQueue.java
+++ test/com/practiceTDD/TestQueue.java
@@ -2,20 +2,36 @@ package com.practiceTDD;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestQueue {
-    Queue queue = new Queue();
+    Queue queue = new Queue(2);
     int[] testValues;
 
     @Test
-    public void enqueue(){
-
+    public void testEnqueue(){
+        assertTrue(queue.enqueue(10));
+        assertTrue(queue.enqueue(20));
     }
 
     @Test
     public void isQueueEmpty(){
         assertEquals(queue.peek(), 0);
+    }
+
+    @Test
+    public void testDequeue(){
+        queue.enqueue(14);
+        assertEquals(14, queue.dequeue());
+        queue.enqueue(12);
+        queue.enqueue(32);
+        assertEquals(12, queue.dequeue());
+    }
+
+    @Test
+    public void checkIsQueueNull(){
+        assertThrows(NullPointerException.class, () -> {
+            queue.dequeue();
+        });
     }
 }
